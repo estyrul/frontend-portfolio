d50440d5957cc9c4d185327fe68cc4b984a96182
Esther Agyekum <EGH00323@SURFLINEGH.LOCAL>
2020-08-28 08:49:53 +0000

    commit_quizapp

---
+++ quizapp/question.js
@@ -0,0 +1,156 @@
+var questions = [
+{
+
+"question": "Choose the odd one out.",
+
+"option1": "csh",
+
+"option2": "bsh",
+
+"option3": "ksh",
+
+"option4": "Kernel",
+
+"answer" : "4",
+},
+
+{
+
+"question": "What is the option to create symbolic link for a file?",
+
+"option1": "–s",
+
+"option2": "–l",
+
+"option3": "–f",
+
+"option4": "None of the above.",
+
+"answer" : "1",
+},
+
+
+{
+
+"question": "What is the option to create hard link for a directory?",
+
+"option1": "–s",
+
+"option2": "–l",
+
+"option3": "–f",
+
+"option4": "None of the above.",
+
+"answer" : "4",
+},
+
+
+{
+
+"question": "The following command can list out all the current active logins.",
+
+"option1": "whoami",
+
+"option2": "who am i",
+
+"option3": "who",
+
+"option4": "None of the above.",
+
+"answer" : "3",
+},
+
+
+{
+"question": "Choose the command to print the current working directory",
+
+"option1": "cwd",
+
+"option2": "pwd",
+
+"option3": "wd",
+
+"option4": "nwd",
+
+"answer" : "2",
+},
+
+
+{
+"question": "What is the file descriptor number which represents the STDOUT?",
+
+"option1": "0",
+
+"option2": "1",
+
+"option3": "2",
+
+"option4": "None of the above.",
+
+"answer" : "2",
+},
+
+
+{
+"question": "What does the following command would do $ cp ../file.",
+
+"option1": "Copies the file from current directory to parent directory",
+
+"option2": "Copies the file from parent directory to its parent directory",
+
+"option3": "Copies the file from parent directory to current directory",
+
+"option4": "Error in the command",
+
+"answer" : "3",
+
+},
+
+
+{
+
+"question": "Identify the environment variable which designates secondary prompt.",
+
+"option1": "PP1",
+
+"option2": "SP2",
+
+"option3": "PS1",
+
+"option4": "PS2",
+
+"answer" : "4",
+},
+
+
+{
+
+"question": "A process is ___",
+
+"option1": "Collection of threads",
+
+"option2": "A thread",
+
+"option3": "Software",
+
+"option4": "A running program",
+
+"answer" : "4",
+},
+
+{
+
+"question": "What is PPID?",
+
+"option1": "Previous process identification",
+
+"option2": "Priority process identification",
+
+"option3": "Parent process identification",
+
+"option4": "Pre Parent identification",
+
+"answer" : "3",
+}
+]
